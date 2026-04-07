84983e11ad96024adb86d0c7ad8541e541c8101c
kashakbayev <kashakbayevaidos@gmail.com>
2026-04-08 01:07:10 +0500

    header is changed

--- src/components/SchoolHub.tsx
+++ src/components/SchoolHub.tsx
@@ -99,7 +99,7 @@ const SchoolHub: React.FC = () => {
       style={{ background: 'linear-gradient(180deg, hsl(220 25% 10%), hsl(220 20% 16%), hsl(25 20% 14%))' }}
     >
       <div className="p-4">
-        <HudBar title="Білім мектебі" />
+        <HudBar title="Edu World" />
       </div>
 
       <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8">
@@ -107,7 +107,7 @@ const SchoolHub: React.FC = () => {
         <div className="text-center mb-10 animate-fade-in">
           <div className="text-6xl mb-3">🏫</div>
           <h1 className="font-display text-4xl font-black mb-2" style={{ color: 'hsl(0 0% 92%)' }}>
-            Білім мектебі
+            Edu World
           </h1>
           <p className="font-body text-lg" style={{ color: 'hsl(220 15% 60%)' }}>
             Кабинетті таңда да, оқуды баста
